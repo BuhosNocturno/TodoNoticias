de0c66a00d5e016dc16176de72a9a17e942d6880
Joel Lopez <119460504+djoel95@users.noreply.github.com>
2023-08-05 14:30:20 -0400

    Feature Body
    -img
    -text
    -css

--- Project/js/home.js
+++ Project/js/home.js
@@ -1,5 +1,6 @@
 const executeInjectHome = async () => {
     await injectHTML('./templates/header.html', document.getElementById('bodyInject'));
+    await injectHTML('./templates/body.html', document.getElementById('bodyInject'));
     await injectHTML('./templates/footer.html', document.getElementById('bodyInject'));
   }
   executeInjectHome()
